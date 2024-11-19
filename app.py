a843653ca566e7b4f4bc2dc6b64fe9f00b210658
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-11-19 16:42:13 -0300

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
-from models import db
+from flask import Flask, request, jsonify
+from models.user import db
 app = Flask(__name__)
 
-@app.route('/api')
-def teste(): 
-    return 'ola mundo'
+@app.route('/api', methods=['POST'])
+def cadastrar(): 
+    data = request.json
+    nome = data.get('nome')
+    return jsonify({"nome":nome, "mensagem":"oi pessoa"})
 
 if __name__=='__main__':
     app.run(debug=True)
