ac9fd8e561a6fc0a3842ede480f53dad24dadf2d
ErickVerissimoo <erickverissimodasilva144@gmail.com>
2024-11-19 18:04:40 -0300

    database e models criados

--- app.py
+++ app.py
@@ -6,12 +6,7 @@ app = create_app()
 @app.route('/api', methods=['POST'])
 def cadastrar(): 
     data = request.json
-    usee = User()
-    usee.email = data.get('email')
-    usee.username = data.get('username')
-    usee.password = data.get('password')
-    db.session.add(usee)
-    db.session.commit()
+    addUser(data.get('username'), data.get('email'),  data.get('password'))
     return jsonify({"Mensagem": "usuario cadastrado com sucesso!"})
 
 if __name__=='__main__':
--- models/classes.py
+++ models/classes.py
@@ -1,17 +1,42 @@
 from models.database import db
 from sqlalchemy.sql import func
+from voluptuous import Schema, Invalid, Email, Length
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False)
     email= db.Column(db.String(100), nullable=False, unique= True)
     password = db.Column(db.String(50), nullable=False)
-    posts = db.relationship('post', backref='autor', lazy=True)
+    posts = db.relationship('Post', backref='autor', lazy=True) 
     
-class post(db.Model):
+class Post(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(40), nullable=False)
     body = db.Column(db.String(1000), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=func.now)
+    date = db.Column(db.DateTime, nullable=False, default=func.now())
     usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+schema = Schema({
+    'username': Length(min=3),
+    'password': Length(min=9),
+    'email': Email()
+})
+def isValid(username, email, password):
+    try:
+        schema({'username': username, 'password': password, 'email': email})      
+        return True
+    except Invalid as inv:
+        print(f'Dado inválido: {inv}')
+        return False
+    
+def addUser(username, email, password):
+    if isValid(username, email, password):
+        usuario = User()
+        usuario.email=email
+        usuario.password=password
+        usuario.username=username
+        db.session.add(usuario)
+        db.session.commit()
+    else:
+        raise ValueError('Dados inválidos')
